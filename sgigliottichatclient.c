b50ba4e1eb56e25af06ff89f90ddc52d0087c242
Sophia <sgigliotti@westmont.edu>
2019-01-24 15:20:44 -0800

    send message working and receive message working, both need improvement

--- sgigliottichatclient.c
+++ sgigliottichatclient.c
@@ -3,18 +3,27 @@
 #include<unistd.h>
 #include<sys/types.h>
 #include<arpa/inet.h>
+#include <string.h>
 
-//Resources: Kleinberg and Wilkins
+//Resources: Kleinberg and Wilkens
 //https://gist.github.com/jirihnidek/388271b57003c043d322
 //http://man7.org/linux/man-pages/man3/getaddrinfo.3.html
+int receiveMsg(int sock){
+  char rMsg[256];
+  int rec = recv(sock, rMsg, sizeof(rMsg),0);
+  printf("From Server: %s\n", rMsg);
+  return rec;
+}
 
 int connectToServer(){
     //create a socket
     //const struct in_addr add;
     int sock = socket(AF_INET,SOCK_STREAM,0);
+    int BUF_SIZE = 100;
     //used to pass in the IP address
     struct in_addr add;
     struct sockaddr_in saddr;
+
     if(sock){
         //convert the IP address so that it can be passed into
         //the connection
@@ -32,6 +41,32 @@ int connectToServer(){
         int connection = connect(sock, (struct sockaddr*)&saddr, sizeof(saddr));
 
         printf("Connection: %d \n", connection);
+
+        //use send and receive, some sort of while loop to get data
+
+        //ask user for name
+        char user[100];
+        printf("Please enter your username: ");
+        fgets(user,100,stdin);
+
+        //send username to the server
+        int sentUser = send(sock, &user, sizeof(user),0);
+        printf("Sent: %d\n", sentUser);
+
+        char msg[256];
+        for(int i = 0; i < sizeof(msg); i++){
+          msg[i] = ' ';
+        }
+        while(*msg != '\n'){
+          for(int i = 0; i < sizeof(msg); i++){
+            msg[i] = ' ';
+          }
+          fgets(msg,sizeof(msg),stdin);
+          int sentMsg = send(sock, &msg, sizeof(msg),0);
+          receiveMsg(sock);
+        }
+
+        //printf("%s \n", msg);
         //close socket
         int closeSock = close(sock);
 
@@ -40,8 +75,10 @@ int connectToServer(){
     return sock;
 }
 
+
+
 int main(int argc, char *argv[]){
     printf("Test\n");
     int check = connectToServer();
-    printf("Socket closed %d\n",check);
+    printf("Socket closed: %d\n",check);
 }
